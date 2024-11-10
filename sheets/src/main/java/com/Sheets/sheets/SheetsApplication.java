4fd2bc5d74593e26ee368600f60050d18ca2558a
nilabja1 <nilabja.work@gmail.com>
2024-11-10 09:23:32 +0530

    Initial commit of Spring Boot project

---
+++ sheets/src/main/java/com/Sheets/sheets/SheetsApplication.java
@@ -0,0 +1,13 @@
+package com.Sheets.sheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheetsApplication.class, args);
+	}
+
+}
---
+++ sheets/src/test/java/com/Sheets/sheets/SheetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sheets.sheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
